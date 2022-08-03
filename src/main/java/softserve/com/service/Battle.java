28dcddfc47408e611d8fa21075433cb3f9e42f45
bsyso <bsyso@softserveinc.com>
2022-08-03 12:58:06 +0200

    added logger

--- src/main/java/softserve/com/service/Battle.java
+++ src/main/java/softserve/com/service/Battle.java
@@ -1,34 +1,42 @@
 package softserve.com.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import softserve.com.model.entities.Army;
-import softserve.com.model.interfaces.CanFight;
 import softserve.com.model.entities.Warrior;
 
 public class Battle {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Battle.class);
+
     private Battle() {
     }
 
     public static boolean fight(Warrior warrior1, Warrior warrior2) {
-        while (warrior1.isAlive() && warrior2.isAlive()) {
-            warrior1.hit(warrior2);
-            if (warrior2.isAlive()) {
-                warrior2.hit(warrior1);
-            }
-        }
-        return warrior1.isAlive();
-    }
+        LOGGER.debug("Battle between {} and {} have been started, health of first = {}  health of second = {}",
+                warrior1.getClass().getSimpleName(),
+                warrior2.getClass().getSimpleName(),
+                warrior1.getHealth(),
+                warrior2.getHealth()
+        );
 
-    public static boolean fight(CanFight warrior1, CanFight warrior2) {
         while (warrior1.isAlive() && warrior2.isAlive()) {
             warrior1.hit(warrior2);
             if (warrior2.isAlive()) {
                 warrior2.hit(warrior1);
             }
         }
+        LOGGER.debug("Battle between {} and {} have been completed, health after the fight of first = {}  health after the fight of second = {}",
+                warrior1.getClass().getSimpleName(),
+                warrior2.getClass().getSimpleName(),
+                warrior1.getHealth(),
+                warrior2.getHealth()
+        );
+
         return warrior1.isAlive();
     }
 
     public static boolean fight(Army attackerArmy, Army defenderArmy) {
+        LOGGER.debug("Armies battle have been started");
         int lengthFirstArmy = attackerArmy.getWarriors().size() - 1;
         int lengthSecondArmy = defenderArmy.getWarriors().size() - 1;
 
@@ -46,6 +54,8 @@ public class Battle {
                 cursorFirstArmy++;
             }
         }
+        LOGGER.debug("Armies battle have been completed");
         return warriorAttacker.isUnitAlive();
     }
+
 }
